a4301c528f9653150b7c078493474ee1a4a516e7
lxy412 <xiaoyao.luo@hotmail.com>
2021-06-10 15:34:53 +0200

    Initial commit

---
+++ app/src/main/java/nl/tudelft/graduateasap/particlefilter/MainActivity.java
@@ -0,0 +1,14 @@
+package nl.tudelft.graduateasap.particlefilter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
